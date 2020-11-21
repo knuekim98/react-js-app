c69294c2df303200b518fbd1ca8e53dbd0566beb
knuekim98 <knuekim98@naver.com>
2020-11-21 15:42:43 +0900

    feat: anchor, basic design

--- src/App.js
+++ src/App.js
@@ -5,42 +5,74 @@ import {
   Button
 } from 'reactstrap';
 import { Parallax } from 'react-parallax';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 class App extends Component {
   render() {
 
-    const navMenu = {
-      color: "#dddddd",
-      margin: "15px"
+    const anchor = {
+      color: "#dfdfdf",
+      margin: "15px",
+      textDecoration: 'none'
     };
+
+    const titleText = {
+      color: "#ffffff",
+      fontFamily: "Spoqa Han Sans",
+      fontWeight: "bold",
+      fontSize: "3em"
+    }
+    const normalText = {
+      color: "#ffffff",
+      fontFamily: "Spoqa Han Sans"
+    }
 
     return (
       <fragment>
-        <nav style={{textAlign: "center", backgroundColor: '#262a30', padding: '15px', height: '80px'}}>
-          <ul>
-            <ui style={navMenu}>main</ui>
-            <ui style={navMenu}>introduce</ui>
-            <img src={Logo} style={{height: '50px', marginLeft: '15px', marginRight: '15px'}}></img>
-            <ui style={navMenu}>project</ui>
-            <ui style={navMenu}>contact</ui>
-          </ul>
-        </nav>
-        <Parallax blur={0} bgImage={bgImg} bgImageAlt="the cat" strength={-200}>
-          <h2>hi</h2>
-          <p>dfdfdsfsdfasda</p>
-
-          {/*
-          <div>
-            <img src={mainLogo} style={circleImg}></img>
-            <h2>knuekim98</h2>
+        <section id='main'>
+          <nav style={{textAlign: "center", backgroundColor: '#262a30', padding: '15px', height: '80px', position: 'sticky'}}>
+            <ul>
+              <ui>
+                <AnchorLink href="#main" style={anchor}>main</AnchorLink>
+              </ui>
+              <ui>
+                <AnchorLink href="#introduce" style={anchor}>introduce</AnchorLink>
+              </ui>
+              <img src={Logo} style={{height: '50px', marginLeft: '15px', marginRight: '15px'}}></img>
+              <ui>
+                <AnchorLink href="#project" style={anchor}>project</AnchorLink>
+              </ui>
+              <ui>
+                <AnchorLink href="#contact" style={anchor}>contact</AnchorLink>
+              </ui>
+            </ul>
+          </nav>
+          <Parallax blur={0} bgImage={bgImg} strength={-200}>
+              <div style={{width: '100%', height: '855px', textAlign: 'center', display: 'table'}}>
+                <div style={{display: 'table-cell', verticalAlign: 'middle'}}>
+                  <h2 style={titleText}>Welcome!</h2>
+                  <p style={normalText}>Programming, Aritifical Intelligence, Hacking</p>
+                  <hr color="#555555" width="20%"></hr>
+                  <AnchorLink href="#introduce"><Button color="primary" style={{marginTop: "10px"}}>Check it out</Button></AnchorLink>
+                </div>
+              </div>
+          </Parallax>
+        </section>
+        <section id='introduce'>
+          <div style={{width: '100%', height: '500px', backgroundColor: '#222222', textAlign: 'center'}}>
+            <p style={titleText}>Introduce</p>
           </div>
-          <div>
-            hello react
+        </section>
+        <section id='project'>
+          <div style={{width: '100%', height: '500px', backgroundColor: '#222222', textAlign: 'center'}}>
+            <p style={titleText}>project</p>
           </div>
-          <Button color="danger">Danger!</Button>
-          */}
-
-        </Parallax>
+        </section>
+        <section id='contact'>
+          <div style={{width: '100%', height: '500px', backgroundColor: '#222222', textAlign: 'center'}}>
+            <p style={titleText}>contact</p>
+          </div>
+        </section>
       </fragment>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import 'bootstrap/dist/css/bootstrap.css'
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ('Spoqa Han Sans'),
+  },
+});
+
 ReactDOM.render(
-  <App />,
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
